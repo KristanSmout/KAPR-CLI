fd5e5154a53bc7ba369ba0ba61d8c5e35b654087
Kristan Smout <39829356+KristanSmout@users.noreply.github.com>
2023-12-03 22:27:24 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KAPR_CLI
+{
+    internal class Program
+    {
+        // Configuration Variables
+        public static bool debugMode = false;
+        public static bool verboseMode = false;
+
+        //Selenium Variables
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
